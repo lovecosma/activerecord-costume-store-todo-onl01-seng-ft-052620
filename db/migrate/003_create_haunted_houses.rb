778119193bb4b73fdda8d3843cc52335b49cb72f
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-27 18:13:06 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -2,7 +2,10 @@ class CreateHauntedHouse < ActiveRecord::Migration[5.2]
 
   def change
     create_table haunted_houses do |t|
-    end 
+      t.name
+      t.location
+      t.theme
+    end
   end
 
 end
